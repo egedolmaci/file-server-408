a817e0c71e1f716fb08c550e2d9dc5fff791a91c
egedolmaci <dolmaciege@gmail.com>
2024-11-06 23:03:36 +0300

    added file storage functionality

--- client.py
+++ client.py
@@ -1,15 +1,60 @@
 import socket
+import os
 
 HOST = "127.0.0.1"
 PORT = 9876
+
+def file_transfer():
+    try:
+        file_path = input("Please enter the file you want to send: ")
+
+        if not os.path.exists(file_path):
+            raise FileNotFoundError("File does not exist")
+        
+        file = open(file_path, "rb")
+        file_name = os.path.basename(file_path)
+
+        file_size = os.path.getsize(file_path)
+        file_name_size = len(file_name)
+
+        print(f"file_name_size = {file_name_size}")
+        print(f"file_size: {file_size}")
+
+
+        return (file, file_name, file_size, file_name_size)
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
     while True:
-        data = input("enter your data: ")
-        s.sendall(data.encode())
+        try:
+            file, file_name, file_size, file_name_size = file_transfer()
 
-        data = s.recv(1024)
+            length_prefix = file_name_size.to_bytes(4, "big")
+            file_name = file_name.encode()
+            file_name_packet =  length_prefix + file_name
+            s.sendall(file_name_packet)
 
-        print(f"Received: {data.decode()}")
+            file_size_len = len(str(file_size))
+            length_prefix = file_size_len.to_bytes(4, "big")
+            file_size_b = str(file_size).encode()
+            file_size_packet = length_prefix + file_size_b
+            s.sendall(file_size_packet)
+
+            bytes_sent = 0
+            while bytes_sent < file_size:
+                data = file.read(4096)
+                if not data:
+                    break
+                s.sendall(data)
+                bytes_sent += len(data)
+                print(f"Progress: {(bytes_sent/file_size)*100:.2f}%")
+            
+        except Exception as e:
+            print(f"File problem: {e}")
+
+        finally:
+            file.close()
--- server.py
+++ server.py
@@ -1,56 +1,80 @@
 import socket
 import threading
 import time 
+import os
 
 HOST = "127.0.0.1"
 PORT = 9876
 
-client_dict = {}
-client_dict_lock = threading.Lock()
+client_conn_history = {}
+client_conn_history_lock = threading.Lock()
+
+client_file_registry = {}
+client_file_registry = threading.Lock()
+file_storage_path = os.getcwd()
+
+
 
 # def handle_commands():
 #     while True:
         # command = input("Enter a command: ")
         # if command == "list":
-        #     with client_dict_lock:
-        #         if not client_dict:
+        #     with client_conn_history_lock:
+        #         if not client_conn_history:
         #             print("No clients are connected")
         #         else:
         #             print("Clients connected:")
-        #             for addr, info in client_dict.items():
+        #             for addr, info in client_conn_history.items():
         #                 print(f"Client {addr} - Connected at: {info['time']}")
             
 
-def print_client_dict(data):
-    with client_dict_lock:
-        if not client_dict:
+def print_client_conn_history(data):
+    with client_conn_history_lock:
+        if not client_conn_history:
             print("No clients are connected")
         else:
             print("Clients connected:")
-            for addr, info in client_dict.items():
+            for addr, info in client_conn_history.items():
                 print(f"Client {addr} - Connected at: {info['time']}")
 
 
 def client_connection(conn, addr):
     with conn:
-        with client_dict_lock:
-            client_dict[addr] = {
+        with client_conn_history_lock:
+            client_conn_history[addr] = {
                 "connection": conn,
                 "time": time.strftime("%H:%M:%S")
             }
         print(f"connected by: {addr}")
 
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
+            length_prefix = conn.recv(4)
+            file_name_size = int.from_bytes(length_prefix, "big")
 
-            readible_data = data.decode()
-            if readible_data == "list":
-                print_client_dict(data.decode())
+            print(f"file_name_size: {file_name_size}")
 
-            print(f"data received from {addr} is {readible_data}")
-            conn.sendall(data)
+            file_name = conn.recv(file_name_size)
+            print(f"file Name: {file_name.decode()}")
+
+            length_prefix = conn.recv(4)
+            file_size_size = int.from_bytes(length_prefix, "big")
+
+            print(f"file_size_size: {file_size_size}")
+
+            file_size = conn.recv(file_size_size)
+            file_size = int(file_size.decode())
+            print(f"file size: {file_size}")
+
+
+            with open(f"received_{file_name}", "wb") as f:
+                bytes_received = 0
+                while bytes_received < file_size:
+                    data = conn.recv(4096)
+                    if not data:
+                        break
+                    f.write(data)
+                    bytes_received += len(data)
+                    print(f"Progress: {(bytes_received/file_size)*100:.2f}%")
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -66,6 +90,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         conn, addr = s.accept()
 
-        t = threading.Thread(target=client_connection, args=(conn, addr), daemon=True)
+        t = threading.Thread(target=client_connection, args=(conn, addr))
         t.start()
     
