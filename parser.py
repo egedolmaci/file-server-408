f1da17d4d5f2b6cb0ffd59b05b268121413686d4
egeyardimci <ege.yardimci@sabanciuniv.edu>
2024-11-09 19:40:30 +0300

    fixed parser return types added commit

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from socket import socket
+import socket
 
 # COMMANDS
 DOWNLOAD = 1
@@ -12,7 +12,7 @@ OK = 10
 NOK = 11
 
 
-def receive_package(sock: socket) -> str:
+def receive_package(sock: socket.socket) -> str:
     length_prefix = sock.recv(4)
     data_length = int.from_bytes(length_prefix, "big")
     data = sock.recv(data_length)
@@ -20,26 +20,26 @@ def receive_package(sock: socket) -> str:
     return data
 
 
-def send_package(sock: socket, size: int, data: str) -> None:
+def send_package(sock: socket.socket, size: int, data: str) -> None:
     length_prefix = size.to_bytes(4, "big")
     data_b = data.encode()
     data_packet = length_prefix + data_b
     sock.send(data_packet)
 
 
-def send_command(sock: socket, cmd: int) -> None:
+def send_command(sock: socket.socket, cmd: int) -> None:
     sock.send(cmd.to_bytes(4, "big"))
 
 
-def receive_command(sock: socket) -> int:
+def receive_command(sock: socket.socket) -> int:
     cmd = sock.recv(4)
     return int.from_bytes(cmd, "big")
 
 
-def send_acknowledgement(sock: socket, val: int) -> None:
+def send_acknowledgement(sock: socket.socket, val: int) -> None:
     sock.send(val.to_bytes(2, "big"))
 
 
-def receive_acknowledgement(sock: socket) -> int:
+def receive_acknowledgement(sock: socket.socket) -> int:
     val = sock.recv(2)
     return int.from_bytes(val, "big")
